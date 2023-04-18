1c8721885f3ffb229b156cb5fe31dc54041f6909
Chamoda TK <mrchamodatharindu@gmail.com>
2023-04-18 17:09:17 +0530

    uhyj

--- mobile_app_django_restapi/settings.py
+++ mobile_app_django_restapi/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 import django_heroku
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('django-insecure-rq$lj#evap9cv-nk=rdum95g*4&y1-bnos793g(+=&-^kk@58a')
+SECRET_KEY = 'django-insecure-rq$lj#evap9cv-nk=rdum95g*4&y1-bnos793g(+=&-^kk@58a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
