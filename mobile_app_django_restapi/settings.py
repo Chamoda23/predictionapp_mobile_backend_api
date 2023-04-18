e44c0621afdea0da77e66ab9d7b28611f53997cc
Chamoda TK <mrchamodatharindu@gmail.com>
2023-04-18 16:15:32 +0530

    vfbsbsf

--- mobile_app_django_restapi/settings.py
+++ mobile_app_django_restapi/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-rq$lj#evap9cv-nk=rdum95g*4&y1-bnos793g(+=&-^kk@58a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['backend-ap.herokuapp.com']
 
 
 # Application definition
